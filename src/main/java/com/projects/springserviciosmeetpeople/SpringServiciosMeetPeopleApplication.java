7be67d267ed2a58a532ad6c69260f519b48ef0ce
jeyson ortiz perez <jeisonortis12@gmail.com>
2020-10-25 21:44:18 +0100

    se creo el spring boot backend

---
+++ src/main/java/com/projects/springserviciosmeetpeople/SpringServiciosMeetPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.projects.springserviciosmeetpeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiciosMeetPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiciosMeetPeopleApplication.class, args);
+	}
+
+}
